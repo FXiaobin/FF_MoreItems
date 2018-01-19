bcf5de97ddb83a93a1d30950415a5d07552ddbbb
apple <JinGuCaiJing@appledemacbook-pro.local>
2018-01-19 15:30:15 +0800

    Initial Commit

---
+++ FFMoreItems/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFMoreItems
+//
+//  Created by IOS开发 on 2018/1/19.
+//  Copyright © 2018年 YuanHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
